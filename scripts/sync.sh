cff9fb9291b3a469e7748dca4831a39775c723c1
Shazu <shazuxd@proton.me>
2024-07-15 14:47:43 +0530

    Update sync.sh

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sync() {
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelOS-AOSP/manifest -b thirteen
+repo init --depth=1 --no-repo-verify -u https://github.com/shazuxd/manifest -b thirteen
 git clone https://github.com/shazuxd/local_manifests .repo/local_manifests
 repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle -j$(nproc --all)
 repo sync --fail-fast -j2
