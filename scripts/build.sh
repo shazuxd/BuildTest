4d203ccd54b5fc840b8cb8447995b8beef76bb6a
Shazu <shazuxd@proton.me>
2024-07-16 01:37:49 +0530

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,7 +5,6 @@ export CCACHE_DIR=/tmp/ccache
 export CCACHE_EXEC=$(which ccache)
 export USE_CCACHE=1
 export SKIP_ABI_CHECKS=true
-export ALLOW_MISSING_DEPENDENCIES=true
 ccache -M 50G
 ccache -o compression=true
 ccache -z
