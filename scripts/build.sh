be34abb1f9b57ef29ca51399652565df61c1f32b
Shazu <shazuxd@proton.me>
2024-07-16 21:35:25 +0530

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 source build/envsetup.sh
-lunch aosp_pissarro-ap2a-userdebug
+lunch derp_pissarro-user
 export CCACHE_DIR=/tmp/ccache
 export CCACHE_EXEC=$(which ccache)
 export USE_CCACHE=1
 ccache -M 50G
 ccache -o compression=true
 ccache -z
-mka bacon
+mka sepolicy
+mka init
+mka boot
+mka derp
