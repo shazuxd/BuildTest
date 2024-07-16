376fb424240d64d696e4450b777644a7cfec357c
Shazu <shazuxd@proton.me>
2024-07-16 21:28:52 +0530

    [SKIP CI]

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sync() {
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelOS-AOSP/manifest -b fourteen
-git clone https://github.com/shazuxd/local_manifests -b qpr3 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/DerpFest-AOSP/manifest.git -b 13
+git clone https://github.com/shazuxd/local_manifests .repo/local_manifests
 repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle -j$(nproc --all)
 }
 echo "Syncing Rom & Device Sources"
