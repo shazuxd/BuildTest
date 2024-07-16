6f76ad6fe0bf153bd5920d96bf1f4dd8b8a9e3bc
Shazu <shazuxd@proton.me>
2024-07-16 16:02:03 +0530

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 source build/envsetup.sh
-lunch aosp_pissarro-user
+lunch aosp_pissarro-userdebug
 export CCACHE_DIR=/tmp/ccache
 export CCACHE_EXEC=$(which ccache)
 export USE_CCACHE=1
-export SKIP_ABI_CHECKS=true
 ccache -M 50G
 ccache -o compression=true
 ccache -z
-make bacon
+mka bacon
